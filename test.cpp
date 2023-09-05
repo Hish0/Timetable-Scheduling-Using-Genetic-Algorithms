c4ac42e98f95ad50da1d8f03417730fb69621986
Hish0 <96497550+Hish0@users.noreply.github.com>
2023-09-05 21:42:55 +0200

    cleaned the main file

--- test.cpp
+++ test.cpp
@@ -1,20 +1,7 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <map>
 #include <cstdlib> // for rand and srand
 #include <ctime>   // for time
-#include <algorithm>
-#include <random> // for random functions
-#include <sstream>
-#include "Lecturer/Lecturer.h"
-#include "TimeSlot/TimeSlot.h"
-#include "Module/Module.h"
-#include "Venue/Venue.h"
-#include "LecturerTimeSlotPreference/LecturerTimeSlotPreference.h"
-#include "ScheduledModule/ScheduledModule.h"
-#include "Chromosome/Chromosome.h"
-#include "GlobalVariables/GlobalVariables.h"
+
 #include "GeneticAlgorithm/GeneticAlgorithm.h"
 
 using namespace std;
@@ -26,11 +13,9 @@ vector<Chromosome> initialPopulation;
 
 int main()
 {
-    cout << "Seeding random number generator..." << endl;
     // Step 0: Seed the random number generator
     srand(static_cast<unsigned int>(time(nullptr)));
 
-    cout << "Initializing required data..." << endl;
     // Step 1: Initialize Required Data
     initializeTimeSlots(); // Assuming this function initializes all possible time slots
     initializeVenues();    // Assuming this function initializes all possible venues
@@ -44,20 +29,15 @@ int main()
     int ELITE_COUNT = 5;
     int NUM_GENERATIONS = 3;
 
-    // cout << "Initializing first population..." << endl;
     //  Step 2: Initialize the First Population
     Population initialPop;
     initialPop.initializeFirstPopulation(POPULATION_SIZE);
-    // cout << "Initial population size: " << initialPop.getChromosomes().size() << endl;
 
-    cout << "Initializing Genetic Algorithm..." << endl;
     // Step 3: Initialize the Genetic Algorithm
     GeneticAlgorithm GA(initialPop, MUTATION_RATE, CROSSOVER_RATE, ELITE_COUNT);
 
-    cout << "Running Genetic Algorithm..." << endl;
     // Step 4: Run the Genetic Algorithm
     GA.run(NUM_GENERATIONS);
-    cout << "Genetic Algorithm completed." << endl;
 
     // Step 5: Evaluate and Display the Best Solution Found
     Chromosome bestChromosome = GA.getBestChromosome(); // Assuming you add a function to return the best chromosome
@@ -77,8 +57,6 @@ int main()
     // Evaluate fitness of the best chromosome
     double fitness = bestChromosome.evaluateFitness();
     cout << "Best Chromosome Fitness: " << fitness << endl;
-    bestChromosome.checkGeneSize();
-    bestChromosome.printGenesByDay();
 
     GA.printTotalFitnessHistory();
 
